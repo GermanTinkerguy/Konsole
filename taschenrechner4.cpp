0d6ab297fae50d9a7280d1145f4a5a7ab19cc7e3
GermanTinkerguy <159229877+GermanTinkerguy@users.noreply.github.com>
2024-02-09 13:45:01 +0100

    Update taschenrechner4.cpp

--- taschenrechner4.cpp
+++ taschenrechner4.cpp
@@ -1,13 +1,15 @@
 #include <iostream>, <cstdlib>
+#include <iomanip>																					// Bib für E Notation
 
 // Variablen
-int wert1 = 0, wert2 = 0, erg = 0, rest = 0;
-char rechenart = 0;
-char prozent = 0;
+long double wert1 = 0.0, wert2 = 0.0, ergebnis = 0.0;
+char rechenart, rechenzeichen;
+bool prozent;
+
 
 int main() {
-
-	{
+		std::cout << std::setiosflags(std::ios_base::fixed) << std::setprecision(0);				// Deaktivierung der E-Notation bzw der Scientific Notation
+	
 		std::cout << "**************************************************************\n";
 		std::cout << "\n";
 		std::cout << "Taschenrechner V1.00\n";
@@ -17,54 +19,71 @@ int main() {
 		std::cout << "\n";
 		std::cout << "Anleitung:\n";
 		std::cout << "\n";
-		std::cout << "Der Rechner verwendet fuer unser deutsches Komma einen Punkt!\n";
+		std::cout << "- Der Rechner verwendet fuer Komma einen Punkt!\n";
+		std::cout << "- Variable als long double deklariert, deswegen keine Werte mit\n";
+		std::cout << "  mehr als 19 Stellen verwenden!\n";
+		std::cout << "- Du willst beispielsweise ausrechnen, wie viel 40% von 170\n";
+		std::cout << "  sind, dann tippe in den Rechner: '170*40% .\n";
 		std::cout << "\n";
-//		std::cout << "Du willst beispielsweise ausrechnen wie viel 40% von 170 sind.\n";
-//		std::cout << "Dann tippe in den Taschenrechner: 170 * 40 % .\n";
-//		std::cout << "\n";
 		std::cout << "**************************************************************\n";
+		std::cout << "\n";
+
+		// Term einlesen
+		std::cout << "Rechenterm eingeben:\n";
+		std::cout << "z.B.: '40+2='\n";
 		std::cout << "\n";
 
 		// Programm wird neu gestartet
 		while (true) {
 
-			// Term einlesen
-			std::cout << "Rechenterm eingeben:\n";
+			std::cin >> wert1 >> rechenart >> wert2 >> rechenzeichen;
 
-			std::cin >> wert1 >> rechenart >> wert2 >> prozent;
+			// Char to Bool um Term mit % zu erkennen
+			if (rechenzeichen == '%') {
+				prozent = true;
 
-			// Berechnung und Anzeige
-			switch (rechenart) {
-			case '+':
-				// Addition
-				erg = wert1 + wert2;
+				ergebnis = wert1 / 100 * wert2;
 				std::cout << "\n";
-				std::cout << wert1 << " + " << wert2 << " = " << erg << "\n";
+				std::cout << wert1 << " x " << wert2 << "% = " << ergebnis << "\n";
 				std::cout << "\n";
-				break;
-			case '-':
-				// Subrtaktion
-				erg = wert1 - wert2;
-				std::cout << "\n";
-				std::cout << wert1 << " - " << wert2 << " = " << erg << "\n";
-				std::cout << "\n";
-				break;
-			case '*':
-				// Multiplikation
-				erg = wert1 * wert2;
-				std::cout << "\n";
-				std::cout << wert1 << " x " << wert2 << " = " << erg << "\n";
-				std::cout << "\n";
-				break;
-			case '/':
-				// Division
-				erg = wert1 / wert2;
-				rest = wert1 % wert2;
-				std::cout << "\n";
-				std::cout << wert1 << " : " << wert2 << " = " << erg << ", Rest " << rest << "\n";
-				std::cout << "\n";
-				break;
+			}
+			if (rechenzeichen == '='){
+				prozent = false;
+
+				// Berechnung und Anzeige
+				switch (rechenart) {
+					case '+':
+						// Addition
+						ergebnis = wert1 + wert2;
+						std::cout << "\n";
+						std::cout << wert1 << " + " << wert2 << " = " << ergebnis << "\n";
+						std::cout << "\n";
+						break;
+					case '-':
+						// Subrtaktion
+						ergebnis = wert1 - wert2;
+						std::cout << "\n";
+						std::cout << wert1 << " - " << wert2 << " = " << ergebnis << "\n";
+						std::cout << "\n";
+						break;
+					case '*':
+					case 'x':
+					case 'X':
+						// Multiplikation
+						ergebnis = wert1 * wert2;
+						std::cout << "\n";
+						std::cout << wert1 << " x " << wert2 << " = " << ergebnis << "\n";
+						std::cout << "\n";
+						break;
+					case '/':
+					case ':':
+						// Division
+						ergebnis = wert1 / wert2;
+						std::cout << "\n";
+						std::cout << wert1 << " : " << wert2 << " = " << ergebnis << "\n";
+						std::cout << "\n";
+						break;
+				}
 			}
 		}
-	}
 }
