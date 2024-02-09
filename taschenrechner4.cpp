e6e97c08e1db9e4d07bdec52fa4d1e44516960c8
GermanTinkerguy <159229877+GermanTinkerguy@users.noreply.github.com>
2024-02-09 10:03:16 +0100

    Update taschenrechner4.cpp

--- taschenrechner4.cpp
+++ taschenrechner4.cpp
@@ -1,55 +1,67 @@
 #include <iostream>, <cstdlib>
 
+// Variablen
+int wert1 = 0, wert2 = 0, erg = 0, rest = 0;
+char rechenart = 0;
+char prozent = 0;
+
 int main() {
 
 	{
+		std::cout << "**************************************************************\n";
+		std::cout << "\n";
 		std::cout << "Taschenrechner V1.00\n";
 		std::cout << "by Oliver\n";
+		std::cout << "\n";
+		std::cout << "**************************************************************\n";
+		std::cout << "\n";
+		std::cout << "Anleitung:\n";
+		std::cout << "\n";
+		std::cout << "Der Rechner verwendet fuer unser deutsches Komma einen Punkt!\n";
+		std::cout << "\n";
+//		std::cout << "Du willst beispielsweise ausrechnen wie viel 40% von 170 sind.\n";
+//		std::cout << "Dann tippe in den Taschenrechner: 170 * 40 % .\n";
+//		std::cout << "\n";
+		std::cout << "**************************************************************\n";
 		std::cout << "\n";
 
 		// Programm wird neu gestartet
 		while (true) {
 
-			// Eingabewerte
-			int wert1 = 0, wert2 = 0;
-			char rechenart = 0;
-
-			// Ergebnisse
-			int sum = 0, sub = 0, mul = 0, div = 0, rest = 0;
-
 			// Term einlesen
 			std::cout << "Rechenterm eingeben:\n";
-			std::cin >> wert1 >> rechenart >> wert2;
+
+			std::cin >> wert1 >> rechenart >> wert2 >> prozent;
 
 			// Berechnung und Anzeige
 			switch (rechenart) {
 			case '+':
 				// Addition
-				sum = wert1 + wert2;
+				erg = wert1 + wert2;
 				std::cout << "\n";
-				std::cout << wert1 << " + " << wert2 << " = " << sum << "\n";
+				std::cout << wert1 << " + " << wert2 << " = " << erg << "\n";
 				std::cout << "\n";
 				break;
 			case '-':
 				// Subrtaktion
-				sub = wert1 - wert2;
+				erg = wert1 - wert2;
 				std::cout << "\n";
-				std::cout << wert1 << " - " << wert2 << " = " << sub << "\n";
+				std::cout << wert1 << " - " << wert2 << " = " << erg << "\n";
 				std::cout << "\n";
 				break;
 			case '*':
 				// Multiplikation
-				mul = wert1 * wert2;
+				erg = wert1 * wert2;
 				std::cout << "\n";
-				std::cout << wert1 << " x " << wert2 << " = " << mul << "\n";
+				std::cout << wert1 << " x " << wert2 << " = " << erg << "\n";
 				std::cout << "\n";
 				break;
 			case '/':
 				// Division
-				div = wert1 / wert2;
+				erg = wert1 / wert2;
 				rest = wert1 % wert2;
 				std::cout << "\n";
-				std::cout << wert1 << " : " << wert2 << " = " << div << ", Rest " << rest << "\n";
+				std::cout << wert1 << " : " << wert2 << " = " << erg << ", Rest " << rest << "\n";
 				std::cout << "\n";
 				break;
 			}
